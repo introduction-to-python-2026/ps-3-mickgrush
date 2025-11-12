cddce4593b08f9f17886dededc7b8d134a5ff81c
mickgrush <mick.grush@gmail.com>
2025-11-12 07:52:33 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,6 +1,6 @@
 def approximate_pi(n_terms):
     pi_final = 0
     for i in range(n_terms):
-        pi_1 = (-1**i)/(2*i+1)
+        pi_1 = (-1)**i/(2*i+1)
         pi_final += pi_1
     return pi_final * 4
